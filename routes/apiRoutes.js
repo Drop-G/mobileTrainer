df38cce99792f3b223ec2444c0cf36fc265f7db8
Ted Peters <44210809+Drop-G@users.noreply.github.com>
2020-11-11 20:59:49 -0500

    cleaned up hw instructions and required html api

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,16 +1,53 @@
-var path = require("path");
+const Workout = require("../models/workout")
 
 module.exports = function(app){ 
-  app.get("/exercise",function (req,res){   
-      res.sendFile(path.join(__dirname,"../public/exercise.html"));
-  });
+    app.get("/api/workouts",function(req,res){  
+        Workout.find()
+        .then(data =>{  
+            res.json(data)
+        })
+        .catch(err => { 
+            res.json(err)
+        })
+    });
 
-  app.get("/",function(req,res){    
-      res.sendFile(path.join(__dirname,"../public/index.html"));
-  });
 
-  app.get("/stats",function(req,res){   
-      res.sendFile(path.join(__dirname,"../public/stats.html"));
-  });
+    app.post("/api/workouts",function (req,res){    
+        Workout.create({})
+        .then(data => res.json(data))
+        .catch(err => { 
+            res.json(err)
+        })
+    });
 
+    app.get("/api/workouts/range",function(req,res){  
+        Workout.find()
+        .then(data =>{  
+            res.json(data)
+        })
+        .catch(err => { 
+            res.json(err)
+        })
+    });
+
+
+    app.post("/api/workouts/range",function (req,res){    
+        Workout.create({})
+        .then(data => res.json(data))
+        .catch(err => { 
+            res.json(err)
+        })
+    });
+
+    app.put("/api/workouts/:id",({body,params},res)=>{   
+        Workout.findByIdAndUpdate(  
+         params.id,
+         {$push:{exercises:body} },
+         {new: true,runValidators:true }
+        )
+        .then(data => res.json(data))
+        .catch(err => { 
+            res.json(err)
+        })
+    });
 }
